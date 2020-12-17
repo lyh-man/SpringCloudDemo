d003846b2d658d24533c4f357af640411e8d1607
liuyihong <liuyihong@odc.gcsoft.com>
2020-12-17 13:18:04 +0800

    在 eureka_client_producer_8002 基础上 配置 actuator、服务发现。 在 eureka_server_7001 基础上，关闭自我保护机制，并配合 eureka_client_producer_8002 配置服务剔除时间。

--- eureka_client_producer_8002/src/main/java/com/lyh/springcloud/eureka_client_producer_8002/EurekaClientProducer8002Application.java
+++ eureka_client_producer_8002/src/main/java/com/lyh/springcloud/eureka_client_producer_8002/EurekaClientProducer8002Application.java
@@ -2,10 +2,12 @@ package com.lyh.springcloud.eureka_client_producer_8002;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class EurekaClientProducer8002Application {
 
     public static void main(String[] args) {
--- eureka_client_producer_8002/src/main/java/com/lyh/springcloud/eureka_client_producer_8002/controller/UserController.java
+++ eureka_client_producer_8002/src/main/java/com/lyh/springcloud/eureka_client_producer_8002/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.lyh.springcloud.eureka_client_producer_8002.controller;
 
-
 import com.lyh.springcloud.common.tools.Result;
 import com.lyh.springcloud.eureka_client_producer_8002.entity.User;
 import com.lyh.springcloud.eureka_client_producer_8002.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/producer/user")
@@ -19,6 +24,23 @@ public class UserController {
 
     @Value("${server.port}")
     private String port;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/discovery")
+    public Result discovery() {
+        // 获取服务名列表
+        List<String> servicesList = discoveryClient.getServices();
+
+        // 根据服务名 获取 每个服务名下的 各个服务的信息
+        Map<String, List<ServiceInstance>> map = new HashMap<>();
+        servicesList.stream().forEach(service -> {
+            map.put(service, discoveryClient.getInstances(service));
+        });
+
+        return Result.ok(true, 200, "discovery services success").data("services", map);
+    }
 
     @GetMapping("/get/{id}")
     public Result getUser(@PathVariable Integer id) {
