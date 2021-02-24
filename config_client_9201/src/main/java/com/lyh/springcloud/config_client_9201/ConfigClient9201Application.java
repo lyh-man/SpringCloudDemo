97ce61b2a06d33b4b4b3c47900be033342806b32
liuyihong <liuyihong@odc.gcsoft.com>
2021-02-24 16:12:03 +0800

    消息总线 SpringCloud Bus   使用 RabbitMQ 作为消息中间件

---
+++ config_client_9201/src/main/java/com/lyh/springcloud/config_client_9201/ConfigClient9201Application.java
@@ -0,0 +1,13 @@
+package com.lyh.springcloud.config_client_9201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient9201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient9201Application.class, args);
+    }
+
+}
---
+++ config_client_9201/src/test/java/com/lyh/springcloud/config_client_9201/ConfigClient9201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyh.springcloud.config_client_9201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClient9201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
